0ee9778d43fc856bfdc4972eb026bdebc78fa506
Alejandro Cervantes <aalejandro.cervantes.ramirez@gmail.com>
2025-12-18 02:16:41 -0600

    arregle loop infinito

--- hola.py
+++ hola.py
@@ -1,8 +1,18 @@
 gastos_total = []
 while True:
     try:
-        gasto = int(input(f"escribe tus gastos: "))
+        gasto = input(f"escribe tus gastos: ")
+        if gasto.lower() == "q":
+            print("haz salido")
+            break
+        gasto = int(gasto) 
         gastos_total.append(gasto)
-    except ValueError:
+    except ValueError: 
         print("valor no valido")
         continue
+total = sum(gastos_total)
+cantidad_gastos = len(gastos_total)
+promedio = (total / cantidad_gastos)
+print(f"este es el total{total:.2f}")
+print(f"este es tu promedio {promedio:.2f}")
+print(f"esta es tu cantidad de gastos {cantidad_gastos}")
